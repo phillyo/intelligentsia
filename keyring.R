cc8f21e572bd34debe3d2b4a13e12844f4605355
Phil <phillyo@users.noreply.github.com>
2019-03-04 22:23:44 +0100

    Delete keyring.R

--- keyring.R
+++
@@ -1,4 +0,0 @@
-#   ____________________________________________________________________________
-#   Keyring                                                                 ####
-
-register_google(key = "AIzaSyBDChCVnrpDxAYV5OsXb6VhQhN2GBTVmKE")
